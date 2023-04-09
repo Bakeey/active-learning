f9652d6b583dc73812850dad61ce9b62a3c10303
Klemens Iten <kiten@ethz.ch>
2023-04-08 19:18:38 -0500

    added RK-4 integration for State and Riccati

--- hw1/q3/Riccati.m
+++ hw1/q3/Riccati.m
@@ -18,21 +18,32 @@ classdef Riccati
             if nargin < 2
                 dt = -1E-2; % TODO fix this cluterfuck
             end
-            Phi_dot = obj.dynamics();
+            
+            Y_1 = obj.Phi;
+            Y_2 = obj.Phi + dt/2 * obj.dynamics(Y_1);
+            Y_3 = obj.Phi + dt/2 * obj.dynamics(Y_2);
+            Y_4 = obj.Phi + dt * obj.dynamics(Y_3);
+
+            % Phi_dot = (obj.dynamics(Y_1)+2*obj.dynamics(Y_2)+2*obj.dynamics(Y_3)+obj.dynamics(Y_4))/6;
+            Phi_dot = obj.dynamics(obj.Phi); 
             phi_prev = obj.Phi + dt*Phi_dot;
             time_prev = obj.time + dt;
             previous_state = Riccati(phi_prev, time_prev);
         end
 
-        function [state_derivative] = dynamics(obj)
+        function [state_derivative] = dynamics(obj, Phi0)
         %DYNAMICS - Dynamics of given system.
         %Returns state derivative
+            if nargin < 1
+                Phi0 = obj.Phi;
+            end
+
             A = [0, 1; -1.6, -0.4];
             B = [0; 1];
             Q    = diag([2,0.01]);
             R    = diag(0.1);
 
-            state_derivative = obj.Phi*B*inv(R)*B'*obj.Phi - obj.Phi*A - A'*obj.Phi-Q;
+            state_derivative = Phi0*B*inv(R)*B'*Phi0 - Phi0*A - A'*Phi0-Q;
         end
 
         function state_vector = to_double(obj)
--- hw1/q3/State.m
+++ hw1/q3/State.m
@@ -25,20 +25,32 @@ classdef State
             if nargin < 2
                 dt = 1E-2;
             end
-            x_dot = obj.dynamics(u);
+               
+            Y_1 = obj.to_double();
+            Y_2 = obj.to_double() + dt/2 * obj.dynamics(u,Y_1);
+            Y_3 = obj.to_double() + dt/2 * obj.dynamics(u,Y_2);
+            Y_4 = obj.to_double() + dt * obj.dynamics(u,Y_3);
+
+            x_dot = (obj.dynamics(u, Y_1) + 2*obj.dynamics(u, Y_2) + ...
+                2*obj.dynamics(u, Y_3) + obj.dynamics(u, Y_4))/6; % RK-4
+
+            % x_dot = obj.dynamics(u, obj.to_double()); % Euler
             x_next = [obj.x; obj.y] + dt*x_dot;
             time_next = obj.time + dt;
             next_state = State(x_next(1,:), x_next(2,:), time_next);
         end
 
-        function [state_derivative] = dynamics(obj,u)
+        function [state_derivative] = dynamics(obj,u, state)
         %DYNAMICS - Dynamics of given linear system.
         %Inputs: 
         % u - current input
         %Returns state derivatives
+            if nargin < 2
+                state = obj.to_double();
+            end
             A = [0, 1; -1.6, -0.4];
             B = [0; 1];
-            state_derivative = A*obj.to_double() + B*u;
+            state_derivative = A*state + B*u;
         end
 
         function state_vector = to_double(obj)
