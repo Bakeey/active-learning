f519d21ef95af04743d0ca8754f3ce734158d7cc
Klemens Iten <kiten@ethz.ch>
2023-04-25 22:35:49 -0500

    HW2 DONE

--- hw2/main.py
+++ hw2/main.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
 @dataclass(frozen=True)
 class Params:
     """Essential parameters for given problem"""
@@ -13,9 +17,9 @@ class Params:
     theta_0: float = np.pi/2
     u_0 = np.array([1, -.5])
 
-    Q = np.diag([100,10,10]) # np.diag([1,3,4])
-    R = np.diag([0.01,0.01]) # np.diag([0.01, 0.01])
-    M = np.diag([0,0,0]) # no terminal cost
+    Q = np.diag([100,10,1]) # np.diag([1,3,4])
+    R = np.diag([0.1,0.1]) # np.diag([0.01, 0.01])
+    M = np.diag([1,0,0]) # no terminal cost
 
     alpha: float = 0.1
     beta: float = 0.5
@@ -75,7 +79,80 @@ class StatePertubation(State):
         assert v.size == 2 , f"Input U has wrong size, expected 2, got: {v.size}"
         next_state: np.ndarray = self() + dt * self.dynamics(v)
         return StatePertubation(next_state, self.t + dt)
+
+
+def plot_initial(state_trajectory: np.ndarray[State], input_trajectory: np.ndarray, initial_trajectory, U_0) -> None:
+    time = [state.t for state in state_trajectory] # np.arange(0,state_trajectory.size)*Params.dt
+    x = [state.x for state in state_trajectory]
+    y = [state.y for state in state_trajectory]
+    theta = [state.theta for state in state_trajectory]
+
+    x_init = [state.x for state in initial_trajectory]
+    y_init = [state.y for state in initial_trajectory]
+    theta_init = [state.theta for state in initial_trajectory]
+
     
+    fig, axs = plt.subplots(2)
+    # fig.suptitle('Vertically stacked subplots')
+    axs[0].plot([0,4],[0,0], 'k--', label='Reference Trajectory')
+    axs[0].plot(x_init,y_init, 'k:', label='Initial Trajectory')
+    axs[0].plot(x,y, 'k', label='Optimized Trajectory')
+    axs[0].set_ylabel('$y$')
+    axs[0].set_xlabel('$x$')
+    axs[0].legend(loc="upper right")
+    axs[1].plot([0,2*np.pi],[np.pi/2,np.pi/2], 'k--', label='Reference Trajectory')
+    axs[1].plot(time, theta_init, 'k:', label='Initial Trajectory')
+    axs[1].plot(time, theta, 'k', label='Optimized Trajectory')
+    axs[1].set_ylabel(r'$\theta$ [rad]')
+    axs[1].set_xlabel('time $t$ [sec]')
+    axs[1].set_ylim([-0.2,2.7])
+    axs[1].legend(loc="upper right")
+    # axs[2].plot(time, U_0[:,0], 'k:', label='Initial Trajectory')
+    # axs[2].plot(time, U_0[:,0], 'k:', label='Initial Trajectory')
+    # axs[2].plot([0,2*np.pi], input_trajectory[:,0], label='Optimized Trajectory')
+    # axs[2].plot([0,2*np.pi], input_trajectory[:,1], label='Optimized Trajectory')
+    # axs[2].set_ylabel('input magnitude')
+    # axs[2].set_xlabel('time $t$ [sec]')
+
+    plt.show()
+    
+    return
+
+
+def plot_optimized(state_trajectory: np.ndarray[State], input_trajectory: np.ndarray, initial_trajectory, U_0) -> None:
+    time = [state.t for state in state_trajectory] # np.arange(0,state_trajectory.size)*Params.dt
+    x = [state.x for state in state_trajectory]
+    y = [state.y for state in state_trajectory]
+    theta = [state.theta for state in state_trajectory]
+
+    x_init = [state.x for state in initial_trajectory]
+    y_init = [state.y for state in initial_trajectory]
+    theta_init = [state.theta for state in initial_trajectory]
+
+    
+    fig, axs = plt.subplots(3)
+    # fig.suptitle('Vertically stacked subplots')
+    axs[0].plot(time,x, 'k', label=r'$x$')
+    axs[0].plot(time,y, 'r', label=r'$y$')
+    axs[0].set_xlabel('time $t$ [sec]')
+    axs[0].set_ylabel('state magnitude')
+    axs[0].legend(loc="upper left")
+    axs[1].plot(time, theta, 'k')
+    axs[1].set_ylabel(r'$\theta$ [rad]')
+    axs[1].set_xlabel('time $t$ [sec]')
+    axs[1].set_ylim([-0.2,2.7])
+    axs[2].plot(time, input_trajectory[:,0], 'k', label=r'$u_0(t)$')
+    axs[2].plot(time, input_trajectory[:,1], 'r', label=r'$u_1(t)$')
+    axs[2].set_ylabel('input magnitude')
+    axs[2].set_xlabel('time $t$ [sec]')
+    axs[2].legend(loc="upper right")
+    axs[2].set_ylim([-7,7])
+
+
+    plt.show()
+    
+    return
+
 
 def plot(state_trajectory: np.ndarray[State], input_trajectory: np.ndarray, initial_trajectory, U_0) -> None:
     time = [state.t for state in state_trajectory] # np.arange(0,state_trajectory.size)*Params.dt
@@ -92,12 +169,19 @@ def plot(state_trajectory: np.ndarray[State], input_trajectory: np.ndarray, init
     # fig.suptitle('Vertically stacked subplots')
     axs[0].plot(x_init,y_init, label = "initial trajectory")
     axs[0].plot(x,y)
+    axs[0].set_ylabel('$y$')
+    axs[0].set_xlabel('$x$')
     axs[1].plot(time, theta_init)
     axs[1].plot(time, theta)
+    axs[1].set_ylabel(r'$\theta$ [rad]')
+    axs[1].set_xlabel('time $t$ [sec]')
     axs[2].plot(time, U_0[:,0])
     axs[2].plot(time, U_0[:,0])
     axs[2].plot(time, input_trajectory[:,0])
     axs[2].plot(time, input_trajectory[:,1])
+    axs[2].set_ylabel('input magnitude')
+    axs[2].set_xlabel('time $t$ [sec]')
+
     plt.show()
     
     return
@@ -281,6 +365,8 @@ def main() -> int:
 
     initial_cost = J(initial_trajectory, U_0)
     initial_deriv = Directional_J(initial_trajectory, U_0, initial_trajectory, U_0)
+    cost = []
+    dcost = []
 
     current_state_trajectory = initial_trajectory
     current_input_trajectory = U_0
@@ -291,7 +377,12 @@ def main() -> int:
     current_state_pertubation[:] = StatePertubation(z_0, t=0)
     counter: int = 0
     while True:
-        print(Directional_J(current_state_trajectory, current_input_trajectory, current_state_pertubation, current_input_pertubation))
+        current_cost= J(current_state_trajectory, current_input_trajectory)
+        current_dcost = Directional_J(current_state_trajectory, current_input_trajectory, current_state_pertubation, current_input_pertubation)
+        cost.append(current_cost)
+        dcost.append(current_dcost)
+        print("    J = ", current_cost)
+        print("    dJ = ",current_dcost)
         if counter > 0 and abs(Directional_J(current_state_trajectory, current_input_trajectory, \
                                          current_state_pertubation, current_input_pertubation)) <= eps:
             break
@@ -332,6 +423,21 @@ def main() -> int:
         print("Current counter: ", counter)
         # end main while loop
     
+    fig, axs = plt.subplots(2)
+    axs[0].plot(np.arange(len(cost)), cost, 'k', label=r'J$(\xi_i)$')
+    axs[0].legend(loc="upper right")
+    axs[0].set_xlabel('iterations')
+    # axs[0].set_ylabel(r'J$(\xi_i)$')
+
+    axs[1].plot(np.arange(len(dcost)-1), [abs(dcost_) for dcost_ in dcost[1:]], 'k',label=r'DJ$(\xi_i)\cdot\zeta$')
+    axs[1].plot([0,(len(dcost)-1)], [1E-4,1E-4], 'k--',label=r'Baseline $10^{-4}$')
+    axs[1].set_yscale('log')
+    axs[1].legend(loc="upper right")
+    # axs[1].set_ylabel(r'DJ$(\xi_i)\cdot\zeta$')
+    axs[1].set_xlabel('iterations')
+
+    plt.show()
+
     plot(current_state_trajectory, current_input_trajectory, initial_trajectory, U_0)
 
     return 1
